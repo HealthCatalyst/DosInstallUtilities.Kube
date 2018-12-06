a5ce41492abc16fe0c1b03481dc6ed0c73803f58
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-12-06 09:54:02 -0800

    fix typo in launching grafana
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- functions/grafana/LaunchGrafana.ps1
+++ functions/grafana/LaunchGrafana.ps1
@@ -35,7 +35,7 @@ function LaunchGrafana()
     Write-Host "Open your browser to http://localhost:3000"
 
     if ($runAsJob) {
-        $sb = [scriptblock]::Create('"kubectl --namespace monitoring port-forward $(kubectl get pod --namespace monitoring -l app=grafana -o template --template "{{(index .items 0).metadata.name}}") 3000:3000')
+        $sb = [scriptblock]::Create('kubectl --namespace monitoring port-forward $(kubectl get pod --namespace monitoring -l app=grafana -o template --template "{{(index .items 0).metadata.name}}") 3000:3000')
         $job = Start-Job -Name "KubGrafana" -ScriptBlock $sb -ErrorAction Stop
         Wait-Job $job -Timeout 5;
         Write-Host "job state: $($job.state)"
